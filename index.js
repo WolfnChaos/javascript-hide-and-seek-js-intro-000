d85c375da78fd616f4fd22379ed82e3b547d3d67
Bradly Horn <wolfnchaos@gmail.com>
2020-08-25 00:20:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
 
-function getFirstSelector(selector) {
-  return document.querySelector(selector);
-}
-
-function nestedTarget() {
-  return document.getElementById('nested').querySelector('ul.ranked-list')
-}
-
-function deepestChild() {
-
-}
-
-function increaseRankBy(n) {
-
-}
